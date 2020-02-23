880aaddf496d2f544b06ddcf0f4e3cb23f3fb25a
Tina Hang <mstinahang@gmail.com>
2020-02-23 00:21:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-function boardMember(name, homeState, training) {
+function BoardMember(name, homeState, training) {
   this.name = name;
   this.homeState = homeState;
   this.training = training;
+}
+
+BoardMember.prototype.veto() {
+  return 'No, I must disagree';
 }
